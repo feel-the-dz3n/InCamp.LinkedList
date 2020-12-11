443bf2d79b083baf0f171a73095564376980816d
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-11 14:01:10 +0200

    Fix code duplication: use getNodeAt instead of loops in each search case

--- src/com/yaroslav/LinkedList.java
+++ src/com/yaroslav/LinkedList.java
@@ -58,75 +58,55 @@ public class LinkedList {
         size++;
     }
 
-    public void insert(String value, int index) {
+    public void insert(String value, int index) throws Exception {
 
         ListNode insertedNode = new ListNode(value);
-        int i = 0;
-        for (ListNode currentNode = head; currentNode != null; currentNode = currentNode.nextNode, i++) {
-            if (i == index) { // found required index
-                insertedNode.prevNode = currentNode.prevNode;
-                insertedNode.nextNode = currentNode;
+        ListNode currentNode = getNodeAt(index);
 
-                if (currentNode.prevNode != null)
-                    currentNode.prevNode.nextNode = insertedNode;
+        insertedNode.prevNode = currentNode.prevNode;
+        insertedNode.nextNode = currentNode;
 
-                currentNode.prevNode = insertedNode;
+        if (currentNode.prevNode != null)
+            currentNode.prevNode.nextNode = insertedNode;
 
-                if (index == 0) head = insertedNode;
-                else if (index == size - 1) tail = insertedNode;
+        currentNode.prevNode = insertedNode;
 
-                break;
-            }
-        }
+        if (index == 0) head = insertedNode;
+        else if (index == size - 1) tail = insertedNode;
 
         size++;
     }
 
     public void removeAt(int index) throws Exception {
-        if (index < 0 || index >= size) throw new Exception("Invalid index");
+        ListNode currentNode = getNodeAt(index);
 
-        int i = 0;
-        for (ListNode currentNode = head; currentNode != null; currentNode = currentNode.nextNode, i++) {
-            if (i == index) { // found required index
-                if (currentNode.prevNode != null)
-                    currentNode.prevNode.nextNode = currentNode.nextNode;
-                else // no prev node, this is the 1st one
-                    head = currentNode.nextNode;
+        if (currentNode.prevNode != null)
+            currentNode.prevNode.nextNode = currentNode.nextNode;
+        else // no prev node, this is the 1st one
+            head = currentNode.nextNode;
 
-                if (currentNode.nextNode != null) {
-                    currentNode.nextNode.prevNode = currentNode.prevNode;
-                } else // no next node, this is the last one
-                    tail = currentNode.prevNode;
-
-                break;
-            }
-        }
+        if (currentNode.nextNode != null) {
+            currentNode.nextNode.prevNode = currentNode.prevNode;
+        } else // no next node, this is the last one
+            tail = currentNode.prevNode;
 
         size--;
     }
 
     public void replace(String newValue, int index) throws Exception {
-        if (index < 0 || index >= size) throw new Exception("Invalid index");
-
         ListNode newNode = new ListNode(newValue);
-        int i = 0;
-        for (ListNode currentNode = head; currentNode != null; currentNode = currentNode.nextNode, i++) {
-            if (i == index) { // found required index
-                if (currentNode.prevNode != null) {
-                    currentNode.prevNode.nextNode = newNode;
-                    newNode.prevNode = currentNode.prevNode;
-                }
-
-                if (currentNode.nextNode != null) {
-                    currentNode.nextNode.prevNode = newNode;
-                    newNode.nextNode = currentNode.nextNode;
-                }
-
-                if (index == 0) head = newNode;
-                else if (index == size - 1) tail = newNode;
-
-                break;
-            }
+        ListNode currentNode = getNodeAt(index);
+        if (currentNode.prevNode != null) {
+            currentNode.prevNode.nextNode = newNode;
+            newNode.prevNode = currentNode.prevNode;
         }
+
+        if (currentNode.nextNode != null) {
+            currentNode.nextNode.prevNode = newNode;
+            newNode.nextNode = currentNode.nextNode;
+        }
+
+        if (index == 0) head = newNode;
+        else if (index == size - 1) tail = newNode;
     }
 }
